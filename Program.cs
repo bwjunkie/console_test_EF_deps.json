203b233263a9e4cdffd4b177673e14b16b34eb61
bwjunkie <josh@coolbluewater.org>
2020-11-12 18:16:20 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_test_EF_deps.json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
